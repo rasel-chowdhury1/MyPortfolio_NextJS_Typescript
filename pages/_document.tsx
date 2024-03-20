de3efd6e2c7c44c4fb3543d2b338ac916ca7a499
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-03-20 12:31:23 +0600

    set title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+
 export default function Document() {
+  
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Rasel Chowdhury Portfolio</title>
+      </Head>
       <body className="font-primary">
         <Main />
         <NextScript />
